9b0381e147cf5a92538e86146a55b2d570e4558d
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-06-03 16:38:24 -0400

    questions change on choice A

--- client/src/component/API.js
+++ client/src/component/API.js
@@ -13,9 +13,11 @@ class API extends Component {
       choiceC: '',
       choiceD: '',
       answer: '',
-      count: 0
+      count: 0,
+      numberOfQuestions: 0,
     };
-    this.callAPI = this.callAPI.bind(this)
+    this.callAPI = this.callAPI.bind(this);
+    this.choiceAClicked = this.choiceAClicked.bind(this);
   }
 
   callAPI() {
@@ -23,6 +25,7 @@ class API extends Component {
     .then(res => res.json())
     .then(res => {
       let apiRespone = res.data;
+      let numberOfQuestions = apiRespone.length - 1;
       let firstQuestion = apiRespone[0].question;
       let answer = apiRespone[0].answer;
       let choiceA = apiRespone[0].choices.a;
@@ -37,7 +40,8 @@ class API extends Component {
         choiceA,
         choiceB,
         choiceC,
-        choiceD
+        choiceD,
+        numberOfQuestions
        });
       // this.state.apiRespone.forEach(res => {
       //   console.log(res.question);
@@ -46,18 +50,63 @@ class API extends Component {
     // .then(res => this.setState({ reapiRespone }))
     // console.log(this.state.apiRespone);
   }
+  choiceAClicked() {
+    if(this.state.answer === "a") {
+      let i = this.state.count;
+      let newQuestionNumber = this.state.questionNumber + 1;
+      i++;
+      let question = this.state.apiRespone[i].question;
+      let answer = this.state.apiRespone[i].answer;
+      let choiceA = this.state.apiRespone[i].choices.a;
+      let choiceB = this.state.apiRespone[i].choices.b;
+      let choiceC = this.state.apiRespone[i].choices.c;
+      let choiceD = this.state.apiRespone[i].choices.d;
+      this.setState({
+        question,
+        answer,
+        choiceA,
+        choiceB,
+        choiceC,
+        choiceD,
+        questionNumber: newQuestionNumber,
+        count: i
+      })
+      this.render()
+    } else {
+      let i = this.state.count;
+      let newQuestionNumber = this.state.questionNumber + 1;
+      i++;
+      let question = this.state.apiRespone[i].question;
+      let answer = this.state.apiRespone[i].answer;
+      let choiceA = this.state.apiRespone[i].choices.a;
+      let choiceB = this.state.apiRespone[i].choices.b;
+      let choiceC = this.state.apiRespone[i].choices.c;
+      let choiceD = this.state.apiRespone[i].choices.d;
+      this.setState({
+        question,
+        answer,
+        choiceA,
+        choiceB,
+        choiceC,
+        choiceD,
+        questionNumber: newQuestionNumber,
+        count: i
+      })
+      this.render()
+    }
+  }
   componentDidMount() {
     window.addEventListener('load', this.callAPI);
- }
+  }
   render() {
   return (
     <div>
-      <div>Question {this.state.questionNumber} / {this.state.apiRespone.length - 1}</div>
+      <div>Question {this.state.questionNumber} / {this.state.numberOfQuestions}</div>
       <div>{this.state.question}</div>
-      <button>{this.state.choiceA}</button>
+      <button onClick={this.choiceAClicked}>{this.state.choiceA}</button>
       <button>{this.state.choiceB}</button>
-      <button>{this.state.choiceC}</button>
-      <button>{this.state.choiceD}</button>
+      {this.state.choiceC === undefined ? <div> </div> : <div><button>{this.state.choiceC}</button>
+      <button>{this.state.choiceD}</button></div>}
     </div>
   );
 }
