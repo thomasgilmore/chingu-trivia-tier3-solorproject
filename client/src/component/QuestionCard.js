207e2807d3895942013e20a18fd70602aff06453
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-06-07 15:35:42 -0400

    disable choices after clicked

--- client/src/component/QuestionCard.js
+++ client/src/component/QuestionCard.js
@@ -6,10 +6,10 @@ function QuestionCard(props) {
     <div className="questionCard">
       <div>Question {props.questionNumber} / {props.numberOfQuestions}</div>
       <h2 className="question">{props.question}</h2>
-      <button onClick={props.choiceAClicked} className="choiceButton">{props.choiceA}</button>
-      <button onClick={props.choiceBClicked} className="choiceButton">{props.choiceB}</button>
-      {props.choiceC === undefined ? <div> </div> : <div><button onClick={props.choiceCClicked} className="choiceButton">{props.choiceC}</button>
-      <button onClick={props.choiceDClicked} className="choiceButton">{props.choiceD}</button></div>}
+      <button disabled={props.choiceAorBorCorDClicked} onClick={props.choiceAClicked} className="choiceButton">{props.choiceA}</button>
+      <button disabled={props.choiceAorBorCorDClicked} onClick={props.choiceBClicked} className="choiceButton">{props.choiceB}</button>
+      {props.choiceC === undefined ? <div> </div> : <div><button disabled={props.choiceAorBorCorDClicked} onClick={props.choiceCClicked} className="choiceButton">{props.choiceC}</button>
+      <button disabled={props.choiceAorBorCorDClicked} onClick={props.choiceDClicked} className="choiceButton">{props.choiceD}</button></div>}
       {props.showAnswerMessage === false ? <div> </div> : <h4 className="answerMessage">{props.answerMessage}</h4>}
       {props.showNextButton === false ? <div> </div> : <div><button onClick={props.nextQuestionClicked} className="nextButton">NEXT</button></div>}
     </div>
