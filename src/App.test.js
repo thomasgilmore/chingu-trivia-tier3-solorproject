b95afcad1f0a0c8fd9b02843c20cef895d9ab28e
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-06-09 16:02:52 -0400

    updated files

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Chingu Trivia title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Chingu Trivia/i);
   expect(linkElement).toBeInTheDocument();
 });
