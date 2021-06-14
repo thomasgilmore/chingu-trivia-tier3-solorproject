2451ee7f2bc594423115bd0033687a8016a5a78e
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-06-14 14:40:38 -0400

    adding comments to server.js

--- server/server.js
+++ server/server.js
@@ -7,10 +7,12 @@ const axios = require('axios');
 
 app.use(express.static(publicPath));
 
+// Getting the build folder and making viewable at http://localhost:3000/
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath));
 });
 
+// Fetching the questions from the API link and also viewbale at http://localhost:3000/api
 app.get('/api', (req, res) => {
     axios.get('https://johnmeade-webdev.github.io/chingu_quiz_api/trial.json')
     .then(response => {
